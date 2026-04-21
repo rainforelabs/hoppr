ce7f89cdcc38c9ebb834fd3653776b28da88af97
adityarohman <aditya@tribel.dev>
2026-04-21 17:49:59 +0800

    Initial Commit

---
+++ Hoppr/HopprApp.swift
@@ -0,0 +1,17 @@
+//
+//  HopprApp.swift
+//  Hoppr
+//
+//  Created by Aditya Rohman on 21/04/26.
+//
+
+import SwiftUI
+
+@main
+struct HopprApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
